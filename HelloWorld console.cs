30d030efe1e2bc9b8d5e816457a2450d67a3d56b
oliviaalchemy <53574712+oliviaalchemy@users.noreply.github.com>
2019-08-02 11:54:44 +0100

    Add files via upload

---
+++ HelloWorld console.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // for writing text 
+
+
+            Console.ReadLine(); // pauses program 
+        }
+    }
+}
